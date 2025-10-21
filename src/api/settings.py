cb842c95a35b18779504df7343890bfdfa9e44bc
lmarti <lucas.marti@fau.de>
2025-10-21 18:19:37 +0200

    new stuff bro

--- src/api/settings.py
+++ src/api/settings.py
@@ -1,2 +1,2 @@
 ONLY_ANSWER = "Do not reformulate the question or try to contextualize. You must only obey the prompt."
-DEFAULT_MODEL = "gemma-3-12b-it-abliterated.q6_k.gguf"
+DEFAULT_MODEL = "Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf"
